ffb773de626b5e98c7e521317a6e92ba81a91fc1
Shelton Francisco <sheltonfjs@gmail.com>
2023-03-11 15:51:11 +0200

    feat: home posts

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,76 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header class="bg-white text-grey-10" bordered>
+      <q-toolbar class="constraint">
+        <q-btn
+          to="/camera"
+          class="large-screen-only q-mr-sm"
+          flat
+          round
+          size="18px"
+          dense
+          icon="eva-camera-outline"
+        />
+        <q-separator
+          vertical
+          spaced
+          class="large-screen-only"
+          />
+        <q-toolbar-title class="text-grand-hotel text-bold">
+          QuasaGram
+        </q-toolbar-title>
+
+        <q-btn
+          to="/"
+          class="large-screen-only"
+          flat
+          round
+          size="18px"
+          dense
+          icon="eva-home-outline"
+        />
+      </q-toolbar>
+    </q-header>
+
+    <q-footer class="bg-white small-screen-only" bordered>
+      <q-tabs
+        class="text-grey-10"
+        active-color="primary"
+        indicator-color="transparent"
+      >
+        <q-route-tab icon="eva-home-outline" to="/" />
+        <q-route-tab icon="eva-camera-outline" to="/camera" />
+      </q-tabs>
+    </q-footer>
+
+    <q-page-container class="bg-grey-3">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+export default {
+  name: "MainLayout",
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="sass">
+.q-toolbar
+  @media (min-width: $breakpoint-sm-min)
+    height: 77px
+
+
+.q-toolbar__title
+  font-size: 30px
+  @media (max-width: $breakpoint-xs-max)
+    text-align: center
+
+.q-tab__icon
+  font-size: 30px
+
+</style>
---
+++ src/pages/PageHome.vue
@@ -0,0 +1,87 @@
+<template>
+  <q-page class="constraint q-pa-md">
+    <q-card
+      v-for="post in posts"
+      :key="post.id"
+      class="my-post q-mb-md"
+      flat
+      bordered
+    >
+      <q-item>
+        <q-item-section avatar>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+          </q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>sheltonfr</q-item-label>
+          <q-item-label caption> {{ post.location }} </q-item-label>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+      <q-img :src="post.imageUrl" />
+      <q-card-section>
+        <div class="">{{ post.caption }}}</div>
+        <div class="text-caption text-grey">{{niceDate(post.date)}}</div>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { date } from "quasar";
+
+export default defineComponent({
+  name: "PageHome",
+
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: "Golden Gate Bridge",
+          date: 1678541530377,
+          location: "South Beach",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 2,
+          caption: "Golden Gate Bridge",
+          date: 1678541530377,
+          location: "South Beach",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 3,
+          caption: "Golden Gate Bridge",
+          date: 1678541530377,
+          location: "South Beach",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+        {
+          id: 4,
+          caption: "Golden Gate Bridge",
+          date: 1678541530377,
+          location: "South Beach",
+          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
+        },
+      ],
+    };
+  },
+
+  methods: {
+    niceDate(value) {
+      return date.formatDate(value, 'YYYY MMMM Do HH:mma');
+    },
+  },
+});
+</script>
+
+<style lang="sass">
+.my-post
+  .q-img__image
+    min-inline-size: 200px
+</style>
