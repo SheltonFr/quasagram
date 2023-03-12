348cd0defb0cf0cbf3d3892b8d7e839580cc0e0f
Shelton Francisco <sheltonfjs@gmail.com>
2023-03-12 10:54:43 +0200

    Camera page layout

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,13 +1,64 @@
 <template>
-  <q-page class="flex flex-center">
-   <h5>Camera Page</h5>
+  <q-page class="constraint-more q-pa-md">
+    <div class="camera-frame q-pa-md">
+      <img class="full-width" src="https://cdn.quasar.dev/img/parallax2.jpg" />
+    </div>
+    <div class="text-center q-pa-md">
+      <q-btn round size="lg" color="grey-10" icon="eva-camera" />
+
+      <div class="row justify-center q-ma-md">
+        <q-input
+          dense
+          class="col col-sm-8"
+          v-model="post.caption"
+          label="Caption" />
+      </div>
+
+      <div class="row justify-center q-ma-md">
+        <q-input
+          dense
+          class="col col-sm-8"
+          v-model="post.location"
+          label="Location">
+          <template v-slot:append>
+            <q-btn round dense flat icon="eva-navigation-2-outline" />
+          </template>
+        </q-input>
+      </div>
+
+      <div class="row justify-center q-mt-lg">
+       <q-btn
+          unelevated
+          rounded
+          color="primary"
+          label="Post Image" />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
+import { defineComponent } from "vue";
+import { uid } from "quasar";
 export default defineComponent({
-  name: 'PageCamera'
-})
+  name: "PageCamera",
+
+  data(){
+    return {
+      post: {
+        id: uid(),
+        caption: "",
+        location: "",
+        photo: null,
+        date: Date.now(),
+      }
+    }
+  }
+});
 </script>
+
+<style lang="sass">
+.camera-frame
+  border: 2px solid $grey-10
+  border-radius: 10px
+</style>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,32 +1,50 @@
 <template>
   <q-page class="constraint q-pa-md">
-    <q-card
-      v-for="post in posts"
-      :key="post.id"
-      class="my-post q-mb-md"
-      flat
-      bordered
-    >
-      <q-item>
-        <q-item-section avatar>
-          <q-avatar>
-            <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-          </q-avatar>
-        </q-item-section>
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-7">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="my-post q-mb-md"
+          flat
+          bordered
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+              </q-avatar>
+            </q-item-section>
 
-        <q-item-section>
-          <q-item-label>sheltonfr</q-item-label>
-          <q-item-label caption> {{ post.location }} </q-item-label>
-        </q-item-section>
-      </q-item>
+            <q-item-section>
+              <q-item-label>sheltonfr</q-item-label>
+              <q-item-label caption> {{ post.location }} </q-item-label>
+            </q-item-section>
+          </q-item>
 
-      <q-separator />
-      <q-img :src="post.imageUrl" />
-      <q-card-section>
-        <div class="">{{ post.caption }}}</div>
-        <div class="text-caption text-grey">{{niceDate(post.date)}}</div>
-      </q-card-section>
-    </q-card>
+          <q-separator />
+          <q-img :src="post.imageUrl" />
+          <q-card-section>
+            <div class="">{{ post.caption }}}</div>
+            <div class="text-caption text-grey">{{ niceDate(post.date) }}</div>
+          </q-card-section>
+        </q-card>
+      </div>
+      <div class="col-5 large-screen-only">
+        <q-item class="fixed">
+            <q-item-section avatar>
+              <q-avatar size="48px">
+                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label>sheltonfr</q-item-label>
+              <q-item-label caption>Teste </q-item-label>
+            </q-item-section>
+          </q-item>
+      </div>
+    </div>
   </q-page>
 </template>
 
@@ -74,7 +92,7 @@ export default defineComponent({
 
   methods: {
     niceDate(value) {
-      return date.formatDate(value, 'YYYY MMMM Do HH:mma');
+      return date.formatDate(value, "YYYY MMMM Do HH:mma");
     },
   },
 });
