92293fca229d59b4a617cad9909ffef07fb809bd
Shelton Francisco <sheltonfjs@gmail.com>
2023-03-12 19:36:40 +0200

    feat: getposts

--- backend/index.js
+++ backend/index.js
@@ -2,23 +2,48 @@
   dependencies
 */
 
-  const express = require('express')
+const express = require('express')
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+const serviceAccount = require('./serviceAccount.json')
 
 /*
   config-express
 */
 
-  const app = express()
+const app = express()
+
 
 /*
-  endpoint:
+  config - firebase
+ */
+
+initializeApp({
+  credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
+/*
+  endpoint - posts
 */
 
-  app.get('/', (request, response) => {
-    response.send('I love Node so Hard')
-  })
+app.get('/posts', async (request, response) => {
+  response.set("Access-Control-Allow-Origin", "*")
+  let posts = []
+  const snapshot = await db.collection('posts').orderBy('date', 'desc').get();
+  snapshot.forEach((doc) => {
+    posts.push(doc.data());
+  });
+
+
+  response.send(posts)
+})
+
 
 /*
   listen
 */
-app.listen(3000)
+
+app.listen(3000, () => console.log("App Running"))
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -2,47 +2,83 @@
   <q-page class="constraint q-pa-md">
     <div class="row q-col-gutter-lg">
       <div class="col-12 col-sm-7">
-        <q-card
-          v-for="post in posts"
-          :key="post.id"
-          class="my-post q-mb-md"
-          flat
-          bordered
-        >
-          <q-item>
-            <q-item-section avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-              </q-avatar>
-            </q-item-section>
+        <template v-if="!loadingPosts && posts.length">
+          <q-card
+            v-for="post in posts"
+            :key="post.id"
+            class="my-post q-mb-md"
+            flat
+            bordered
+          >
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar>
+                  <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+                </q-avatar>
+              </q-item-section>
 
-            <q-item-section>
-              <q-item-label>sheltonfr</q-item-label>
-              <q-item-label caption> {{ post.location }} </q-item-label>
-            </q-item-section>
-          </q-item>
+              <q-item-section>
+                <q-item-label>sheltonfr</q-item-label>
+                <q-item-label caption> {{ post.location }} </q-item-label>
+              </q-item-section>
+            </q-item>
 
-          <q-separator />
-          <q-img :src="post.imageUrl" />
-          <q-card-section>
-            <div class="">{{ post.caption }}}</div>
-            <div class="text-caption text-grey">{{ niceDate(post.date) }}</div>
-          </q-card-section>
-        </q-card>
+            <q-separator />
+            <q-img :src="post.imageUrl" />
+            <q-card-section>
+              <div class="">{{ post.caption }}}</div>
+              <div class="text-caption text-grey">
+                {{ niceDate(post.date) }}
+              </div>
+            </q-card-section>
+          </q-card>
+        </template>
+
+        <template v-else-if="!loadingPosts && !posts.length">
+          <h5 class="text-center text-grey">
+            No posts yet
+          </h5>
+        </template>
+
+        <template v-else>
+          <q-card flat bordered>
+            <q-item>
+              <q-item-section avatar>
+                <q-skeleton type="QAvatar" size="40px" />
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>
+                  <q-skeleton type="text" />
+                </q-item-label>
+                <q-item-label caption>
+                  <q-skeleton type="text" />
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-skeleton height="200px" square />
+
+            <q-card-section>
+              <q-skeleton type="text" class="text-subtitle2" />
+              <q-skeleton type="text" width="50%" class="text-subtitle2" />
+            </q-card-section>
+          </q-card>
+        </template>
       </div>
       <div class="col-5 large-screen-only">
         <q-item class="fixed">
-            <q-item-section avatar>
-              <q-avatar size="48px">
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-              </q-avatar>
-            </q-item-section>
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+          </q-item-section>
 
-            <q-item-section>
-              <q-item-label>sheltonfr</q-item-label>
-              <q-item-label caption>Teste </q-item-label>
-            </q-item-section>
-          </q-item>
+          <q-item-section>
+            <q-item-label>sheltonfr</q-item-label>
+            <q-item-label caption>Teste </q-item-label>
+          </q-item-section>
+        </q-item>
       </div>
     </div>
   </q-page>
@@ -57,36 +93,8 @@ export default defineComponent({
 
   data() {
     return {
-      posts: [
-        {
-          id: 1,
-          caption: "Golden Gate Bridge",
-          date: 1678541530377,
-          location: "South Beach",
-          imageUrl: "https://i.imgur.com/g5hCI2D.jpeg",
-        },
-        {
-          id: 2,
-          caption: "Golden Gate Bridge",
-          date: 1678541530377,
-          location: "South Beach",
-          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
-        },
-        {
-          id: 3,
-          caption: "Golden Gate Bridge",
-          date: 1678541530377,
-          location: "South Beach",
-          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
-        },
-        {
-          id: 4,
-          caption: "Golden Gate Bridge",
-          date: 1678541530377,
-          location: "South Beach",
-          imageUrl: "https://cdn.quasar.dev/img/parallax2.jpg",
-        },
-      ],
+      posts: [],
+      loadingPosts: true,
     };
   },
 
@@ -94,6 +102,33 @@ export default defineComponent({
     niceDate(value) {
       return date.formatDate(value, "YYYY MMMM Do HH:mma");
     },
+
+    getPosts() {
+      // this.$axios.get("https://quasagram-backend.onrender.com/posts")
+      // .then(result => this.posts = result.data)
+      // .catch(error => console.log(error))
+
+      this.$axios
+        .get("https://quasagram-backend.onrender.com/posts")
+        .then((result) => {
+          this.posts = result.data;
+          this.loadingPosts = false;
+
+        })
+        .catch((error) => {
+          this.$q.dialog({
+            title: "Error",
+            message: "An error occurred while readning posts from server!",
+          });
+
+          this.loadingPosts = false;
+        });
+
+    },
+  },
+
+  mounted() {
+    this.getPosts();
   },
 });
 </script>
